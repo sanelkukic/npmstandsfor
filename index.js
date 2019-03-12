a71b6b1b29e17eb5f12cabb32a3d6c1837a441d0
xDrixxyz YT <xd@xdrixxyz.eu.org>
2019-03-12 19:37:25 -0400

    Fixes #1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const __version__ = JSON.parse(fs.readFileSync('package.json')).version;
 const __author__ = JSON.parse(fs.readFileSync('package.json')).author;
 const __issues__ = JSON.parse(fs.readFileSync('package.json')).bugs.url;
 
-if (process.argv[0].toLowerCase() == "--help" || process.argv[0].toLowerCase() == "-h") {
+if (process.argv.indexOf('--help') > -1 || process.argv.indexOf('-h') > -1) {
 	console.log(`npmstandsfor version ${__version__}`);
 	console.log(`Developed by ${__author__}`);
 	console.log('\n');
@@ -15,21 +15,22 @@ if (process.argv[0].toLowerCase() == "--help" || process.argv[0].toLowerCase() =
 	console.log('\n');
 	console.log(`You can report issues to: ${__issues__}`);
 	process.exit(0);
-}
+} else {
 
-var sayings_url = 'https://raw.githubusercontent.com/npm/npm-expansions/master/expansions.txt';
+	var sayings_url = 'https://raw.githubusercontent.com/npm/npm-expansions/master/expansions.txt';
 
-try {
-	axios.get(sayings_url).then(function(resp) {
-		var available_sayings = resp.data;
-		var sayings = available_sayings.split('\n');
-		var random_saying = sayings[Math.floor(Math.random() * sayings.length)];
-		console.log(`npm stands for: ${random_saying}`);
-	});
-} catch (e) {
-	console.log('There was an issue getting a saying.\n');
-	console.log(`You can report this issue to: ${__issues__}`);
-	console.log('\nMake sure you provide the following stack trace');
-	console.log(e.stack);
-	process.exit(1);
+	try {
+		axios.get(sayings_url).then(function(resp) {
+			var available_sayings = resp.data;
+			var sayings = available_sayings.split('\n');
+			var random_saying = sayings[Math.floor(Math.random() * sayings.length)];
+			console.log(`npm stands for: ${random_saying}`);
+		});
+	} catch (e) {
+		console.log('There was an issue getting a saying.\n');
+		console.log(`You can report this issue to: ${__issues__}`);
+		console.log('\nMake sure you provide the following stack trace');
+		console.log(e.stack);
+		process.exit(1);
+	}
 }
